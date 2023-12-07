8613f291d29f900a2dabd0eba6315f34299c9299
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2023-12-07 19:34:27 +0300

    added animations and code improvements

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -10,85 +10,72 @@ import SwiftUI
 
 struct ContentView: View {
     
-
     @State private var score = 0
     @State private var answerCount = 0
     @State private var shouldWin = false
     @State private var isFinish = false
     @State private var computerChoice = LungeTypes.allCases.randomElement() ?? .rock
+    @State private var selectedLunge: LungeTypes?
+
     
-    var question : some View {
-        Text("Whisch one loses ?")
-            .font(.title.weight(.bold))
-            .foregroundStyle(.red)
-    }
-    
-    var question2 : some View {
-        Text("Which one wins ?")
+    var question: some View {
+        Text(shouldWin ? "Which one wins?" : "Which one loses?")
             .font(.title.bold())
-            .foregroundStyle(.green)
+            .foregroundStyle(shouldWin ? .green : .red)
     }
-    
-    
     
     var body: some View {
         
         ZStack {
             Color(red: 0.90, green: 0.90, blue: 0.98)
-                .frame(maxWidth: .infinity , maxHeight: .infinity)
                 .ignoresSafeArea()
-                
+            
             VStack(spacing: 20) {
-                
-                
-                
-              Spacer()
-                
+                Spacer()
+
                 Text("Computer has played")
                     .font(.title.weight(.semibold))
-                   
-               
-                Text(computerChoice.rawValue )
+                
+                Text(computerChoice.rawValue)
                     .font(.system(size: 200))
-               
-             
+                   
                 Group {
-                    if shouldWin == true {
-                        question2
+                    if shouldWin {
+                        question
                     } else {
                         question
                     }
                 }
-    
-            
                 
-
                 HStack(spacing: 20) {
-                    ForEach(LungeTypes.allCases , id: \.self) { lunge in
-                        Button() {
+                    ForEach(LungeTypes.allCases, id: \.self) { lunge in
+                        Button {
                             userSelected(lunge)
+                
                         } label: {
                             Text(lunge.rawValue)
                                 .font(.system(size: 80))
-                         
+                                .rotation3DEffect(
+                                    .degrees(selectedLunge == lunge ? 360 : 0),
+                                    axis: (x: 0, y: 1, z: 0)
+                                )
                         }
                     }
                 }
                 
                 Spacer()
+                
                 Text("Score: \(score)")
                     .font(.title.weight(.semibold))
+                
                 Spacer()
-               
             }
         }
         .alert("Game Over", isPresented: $isFinish) {
-                 Button("Play Again", action: restartGame)
-             } message: {
-                 Text("Your score was \(score)")
-             }
-
-        
+            Button("Play Again", action: restartGame)
+        } message: {
+            Text("Your score was \(score)")
+        }
     }
     
     func userSelected(_ userChoice: LungeTypes) {
@@ -106,13 +93,12 @@ struct ContentView: View {
                 } else {
                     score -= winModifier
                 }
-
-             
             }
             
             computerChoice = LungeTypes.allCases.randomElement() ?? .rock
             answerCount += 1
             shouldWin.toggle()
+            selectedLunge = userChoice
             
         } else {
             isFinish = true
@@ -124,15 +110,6 @@ struct ContentView: View {
         score = 0
         shouldWin = Bool.random()
         computerChoice = LungeTypes.allCases.randomElement() ?? .rock
-        
+        selectedLunge = nil
     }
-    
-    
-
 }
-
-/*
- #Preview {
-     ContentView()
- }
- */
